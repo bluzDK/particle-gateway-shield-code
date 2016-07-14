edeca260c5a4ab3e8ed327ff234d71c39b5bf301
Eric Ely <eric.j.ely@gmail.com>
2016-07-14 00:12:08 -0400

    better example

--- firmware/examples/gateway.cpp
+++ firmware/examples/gateway.cpp
@@ -2,14 +2,13 @@
 #include "bluz_gateway/bluz_gateway.h"
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-bluz_gateway *gateway;
+bluz_gateway gateway;
 
 void setup() {
-    gateway = new bluz_gateway();
-    gateway->init();
+    gateway.init();
 }
 
 void loop() {
-    gateway->loop();
-
+    gateway.loop();
+    
 }
